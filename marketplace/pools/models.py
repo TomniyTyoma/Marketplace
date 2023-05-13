9bef93532c3e7624d9f590799b7087fee75c1a1c
TomniyTyoma <semeykin-artem@mail.ru>
2023-05-13 22:33:50 +0300

    Бесполезный текст

--- marketplace/pools/models.py
+++ marketplace/pools/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=30)
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=19, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.TextField(blank=True, null=True)
 
